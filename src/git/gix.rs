2301405bcc19a3f65567e08a829bbeb540333747
Mika Cohen <mjc@kernel.org>
2026-06-24 23:05:12 -0600

    Remove external gix commit graph smoke test

--- src/git/gix.rs
+++ src/git/gix.rs
@@ -33,24 +33,3 @@ pub fn for_each_branch_tip(repo: &gix::Repository, mut visit: impl FnMut(bool, &
 pub(crate) fn gix_error(error: impl std::fmt::Display) -> git2::Error {
     git2::Error::from_str(&error.to_string())
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::git::test_support::{commit_file, init_repo_at};
-    use std::process::Command;
-
-    #[test]
-    fn commit_graph_helper_uses_written_commit_graph() {
-        let temp = tempfile::Builder::new().prefix("guitar-gix-commit-graph-").tempdir().unwrap();
-        let repo = init_repo_at(temp.path());
-        commit_file(&repo, "one.txt", "one\n", "one");
-        commit_file(&repo, "two.txt", "two\n", "two");
-
-        let status = Command::new("git").arg("-C").arg(temp.path()).args(["commit-graph", "write", "--reachable"]).status().unwrap();
-        assert!(status.success());
-
-        let gix_repo = gix::open(temp.path()).unwrap();
-        assert!(commit_graph_if_available(&gix_repo).is_some());
-    }
-}
