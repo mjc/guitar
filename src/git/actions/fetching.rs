4a08c89e4f2dbc1a85e940af23742fd911cbe30d
Mika Cohen <mjc@kernel.org>
2026-06-25 20:27:30 -0600

    Inline fetch_remote worker body
    (cherry picked from commit 9d57a259cd4c1d48d6086e601cdde8586142c30d)

--- src/git/actions/fetching.rs
+++ src/git/actions/fetching.rs
@@ -1,4 +1,5 @@
 use crate::{
+    git::actions::gix_support::{open_repo_path, to_git2_error},
     git::auth::{AuthAttempt, AuthSession, NetworkResult, network_result},
     helpers::localisation::network,
 };
@@ -13,27 +14,27 @@ pub fn fetch_remote(repo_path: &str, remote_name: &str, auth_session: AuthSessio
     thread::spawn(move || {
         let attempt = AuthAttempt::new(auth_session, network::FETCH());
         let result = (|| -> Result<(), git2::Error> {
-            let mut repo = gix::open(&repo_path).map_err(|error| git2::Error::from_str(&error.to_string()))?;
-            repo.committer_or_set_generic_fallback().map_err(|error| git2::Error::from_str(&error.to_string()))?;
+            let mut repo = open_repo_path(&repo_path)?;
+            repo.committer_or_set_generic_fallback().map_err(to_git2_error)?;
 
             let remote_name = remote_name.as_str();
-            let mut remote = repo.find_remote(remote_name).map_err(|error| git2::Error::from_str(&error.to_string()))?;
+            let mut remote = repo.find_remote(remote_name).map_err(to_git2_error)?;
             let remote_url = remote.url(gix::remote::Direction::Fetch).ok_or_else(|| git2::Error::from_str("Remote URL is missing"))?.to_owned();
 
             remote = remote.with_fetch_tags(gix::remote::fetch::Tags::All);
             let heads = format!("+refs/heads/*:refs/remotes/{remote_name}/*");
-            remote = remote.with_refspecs(Some(heads.as_str()), gix::remote::Direction::Fetch).map_err(|error| git2::Error::from_str(&error.to_string()))?;
-            remote = remote.with_refspecs(Some("refs/tags/*:refs/tags/*"), gix::remote::Direction::Fetch).map_err(|error| git2::Error::from_str(&error.to_string()))?;
+            remote = remote.with_refspecs(Some(heads.as_str()), gix::remote::Direction::Fetch).map_err(to_git2_error)?;
+            remote = remote.with_refspecs(Some("refs/tags/*:refs/tags/*"), gix::remote::Direction::Fetch).map_err(to_git2_error)?;
 
-            let mut connection = remote.connect(gix::remote::Direction::Fetch).map_err(|error| git2::Error::from_str(&error.to_string()))?;
-            let mut configured_credentials = connection.configured_credentials(remote_url).map_err(|error| git2::Error::from_str(&error.to_string()))?;
+            let mut connection = remote.connect(gix::remote::Direction::Fetch).map_err(to_git2_error)?;
+            let mut configured_credentials = connection.configured_credentials(remote_url).map_err(to_git2_error)?;
             let auth = attempt.clone();
             connection.set_credentials(move |action| auth.gix_credentials_with(action, &mut configured_credentials));
 
             let mut progress = gix::progress::Discard;
-            let pending_pack = connection.prepare_fetch(&mut progress, Default::default()).map_err(|error| git2::Error::from_str(&error.to_string()))?;
+            let pending_pack = connection.prepare_fetch(&mut progress, Default::default()).map_err(to_git2_error)?;
             let should_interrupt = std::sync::atomic::AtomicBool::new(false);
-            pending_pack.receive(&mut progress, &should_interrupt).map_err(|error| git2::Error::from_str(&error.to_string()))?;
+            pending_pack.receive(&mut progress, &should_interrupt).map_err(to_git2_error)?;
             Ok(())
         })();
 
