cd36efa482bddd83c93157f51c915df2e1eb489a
Mika Cohen <mjc@kernel.org>
2026-06-25 21:23:30 -0600

    Refactor branch checkout lookup flow
    (cherry picked from commit f956dccc4b8ec500d696f4b404ec7e623428a216)

--- src/git/actions/checkout.rs
+++ src/git/actions/checkout.rs
@@ -59,41 +59,70 @@ pub fn checkout_head(repo: &Repository, oid: Oid) -> Result<(), git2::Error> {
     checkout_tree(&mut repo, tree_id, false)
 }
 
+enum BranchCheckout<'a> {
+    Existing { ref_name: FullName, visible_name: &'a str },
+    Remote { local_ref_name: FullName, remote_ref_name: FullName, remote: &'a str, branch: &'a str },
+}
+
+enum BranchLookup<'a> {
+    Local { branch: &'a str },
+    Remote { branch_name: &'a str, remote: &'a str, branch: &'a str },
+}
+
+impl<'a> BranchCheckout<'a> {
+    fn apply(self, repo: &mut gix::Repository, local: &mut HashMap<u32, Vec<String>>, alias: u32) -> Result<&'a str, git2::Error> {
+        match self {
+            BranchCheckout::Existing { ref_name, visible_name } => checkout_existing_branch(repo, &ref_name).map(|()| visible_name),
+            BranchCheckout::Remote { local_ref_name, remote_ref_name, remote, branch } => {
+                checkout_remote_branch(repo, &local_ref_name, &remote_ref_name, remote, branch, local, alias).map(|()| branch)
+            },
+        }
+    }
+}
+
+fn reference_exists(repo: &gix::Repository, ref_name: &FullName) -> Result<bool, git2::Error> {
+    repo.try_find_reference(ref_name.as_ref()).map(|reference| reference.is_some()).map_err(to_git2_error)
+}
+
+impl<'a> BranchLookup<'a> {
+    fn candidate(self, repo: &gix::Repository) -> Result<Option<BranchCheckout<'a>>, git2::Error> {
+        match self {
+            BranchLookup::Local { branch } => {
+                let ref_name = branch_ref_name(branch)?;
+                Ok(reference_exists(repo, &ref_name)?.then_some(BranchCheckout::Existing { ref_name, visible_name: branch }))
+            },
+            BranchLookup::Remote { branch_name, remote, branch } => {
+                let local_ref_name = branch_ref_name(branch)?;
+                let remote_ref_name = (!reference_exists(repo, &local_ref_name)?).then(|| remote_tracking_ref_name(branch_name)).transpose()?;
+
+                match remote_ref_name {
+                    None => Ok(Some(BranchCheckout::Existing { ref_name: local_ref_name, visible_name: branch })),
+                    Some(remote_ref_name) => Ok(reference_exists(repo, &remote_ref_name)?.then_some(BranchCheckout::Remote { local_ref_name, remote_ref_name, remote, branch })),
+                }
+            },
+        }
+    }
+}
+
+fn branch_lookups(branch_name: &str) -> impl Iterator<Item = BranchLookup<'_>> {
+    let local = std::iter::once(BranchLookup::Local { branch: branch_name });
+    let remote = branch_name.split_once('/').into_iter().map(|(remote, branch)| BranchLookup::Remote { branch_name, remote, branch });
+
+    local.chain(remote)
+}
+
+fn branch_checkout_candidate<'a>(repo: &gix::Repository, branch_name: &'a str) -> Result<Option<BranchCheckout<'a>>, git2::Error> {
+    branch_lookups(branch_name).find_map(|lookup| lookup.candidate(repo).transpose()).transpose()
+}
+
 pub fn checkout_branch(repo: &Repository, hidden_branch_names: &mut HashSet<String>, local: &mut HashMap<u32, Vec<String>>, alias: u32, branch_name: &str) -> Result<(), git2::Error> {
     let mut repo = open_worktree_repo(repo)?;
     repo.committer_or_set_generic_fallback().map_err(to_git2_error)?;
 
-    let local_branch_name = branch_ref_name(branch_name)?;
-    if repo.try_find_reference(local_branch_name.as_ref()).map_err(to_git2_error)?.is_some() {
-        let result = checkout_existing_branch(&mut repo, &local_branch_name);
-        if result.is_ok() {
-            hidden_branch_names.remove(branch_name);
-        }
-        return result;
-    }
-
-    if let Some((remote, branch)) = branch_name.split_once('/') {
-        let local_branch_ref_name = branch_ref_name(branch)?;
-        if repo.try_find_reference(local_branch_ref_name.as_ref()).map_err(to_git2_error)?.is_some() {
-            let result = checkout_existing_branch(&mut repo, &local_branch_ref_name);
-            if result.is_ok() {
-                hidden_branch_names.remove(branch);
-            }
-            return result;
-        }
-
-        let remote_ref_name = remote_tracking_ref_name(branch_name)?;
-        if repo.try_find_reference(remote_ref_name.as_ref()).map_err(to_git2_error)?.is_some() {
-            let result = checkout_remote_branch(&mut repo, &local_branch_ref_name, &remote_ref_name, remote, branch, local, alias);
-            if result.is_ok() {
-                // The checked-out local branch should remain visible under the hide-layer model.
-                hidden_branch_names.remove(branch);
-            }
-            return result;
-        }
-    }
-
-    Err(git2::Error::from_str("No matching local or remote branch found"))
+    let candidate = branch_checkout_candidate(&repo, branch_name)?.ok_or_else(|| git2::Error::from_str("No matching local or remote branch found"))?;
+    let visible_name = candidate.apply(&mut repo, local, alias)?;
+    hidden_branch_names.remove(visible_name);
+    Ok(())
 }
 
 #[cfg(test)]
